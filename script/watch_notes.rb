369cabdd88ba9156eb0175e8a5319ece7b35b5fe
Syd <hello.sydd@gmail.com>
2012-11-16 01:02:22 +0800

    update file watcher paths

--- script/watch_notes.rb
+++ script/watch_notes.rb
@@ -1,18 +1,17 @@
 # encoding: utf-8
+require 'yaml'
 require 'fileutils'
 require 'rb-fsevent'
 
 script_dir = File.dirname(File.expand_path(__FILE__))
-
-HOME = ENV['HOME']
-paths = ["#{HOME}/Programming/rails/markdown_notes/source", "#{HOME}/Library/Application Support/Notational Data"]
-options = ['--latency', 1.5.to_s, '--no-defer']
+markdown_dirs = YAML.load_file('config/nvalt.yml')['markdown_dir']
+options = ['--latency', 1.0.to_s, '--no-defer']
 
 MARKDOWN_RE = /\.(md|markdown|txt)$/
 
 begin_time = Time.now
 fsevent = FSEvent.new
-fsevent.watch paths, options do |directories|
+fsevent.watch markdown_dirs, options do |directories|
   directories.each do |directory|
     Dir.entries(directory).grep(MARKDOWN_RE).each do |file|
       file_name = File.join(directory, file)
