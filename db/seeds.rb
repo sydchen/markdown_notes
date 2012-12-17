7ae5035b225cda4180a28965f572f392c05be6f4
Syd Chen <schen@optimiscorp.com>
2012-12-17 11:41:31 +0800

    Using Dir.glob recursively get all files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
 # encoding: utf-8
 require 'fileutils'
 
-MARKDOWN_RE = /\.(md|markdown|txt)$/
+#MARKDOWN_RE = /\.(md|markdown|txt)$/
 
 def generate
   markdown_dirs = YAML.load_file('config/nvalt.yml')['markdown_dir']
   markdown_dirs.each do |markdown_dir|
-    markdown_files = Dir.entries(markdown_dir).grep(MARKDOWN_RE)
+    #markdown_files = Dir.glob(File.join(markdown_dir, '**')).grep(MARKDOWN_RE)
+    markdown_files = Dir.glob(File.join(markdown_dir, '**', '*.{md,markdown,txt}'))
     markdown_files.each do |file_name|
-      Note.from_file(File.join(markdown_dir, file_name), file_name)  
+      Note.from_file(file_name, File.basename(file_name))  
     end
   end  
 end
